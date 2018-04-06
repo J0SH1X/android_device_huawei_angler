cc7e929cbe7e656eb519832be1b71ee6643abb4c
J0SH1X <aljoshua.hell@gmail.com>
2018-04-06 12:14:52 +0200

    upload to github

---
+++ config.fs
@@ -0,0 +1,10 @@
+[vendor/bin/qmuxd]
+mode: 0700
+user: AID_RADIO
+group: AID_SHELL
+caps: BLOCK_SUSPEND
+[vendor/bin/mm-qcamera-daemon]
+mode: 0700
+user: AID_CAMERA
+group: AID_SHELL
+caps: SYS_NICE
