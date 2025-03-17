9e1f6df1d5c1f85a31bcdd24999c51fe9d3bd3c9
J0SH1X <aljoshua.hell@gmail.com>
2025-03-17 19:25:43 +0100

    msm8994-common: decommunize wifi

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -161,18 +161,5 @@ SELINUX_IGNORE_NEVERALLOWS_ON_USER := true
 BOARD_SEPOLICY_DIRS += device/huawei/msm8994-common/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += device/huawei/msm8994-common/sepolicy/private
 
-# Wifi related defines
-BOARD_WPA_SUPPLICANT_DRIVER := NL80211
-WPA_SUPPLICANT_VERSION := VER_0_8_X
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
-BOARD_HOSTAPD_DRIVER := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_bcmdhd
-BOARD_WLAN_DEVICE := bcmdhd
-WIFI_DRIVER_FW_PATH_PARAM := "/sys/module/bcmdhd/parameters/firmware_path"
-WIFI_DRIVER_FW_PATH_STA := "/vendor/firmware/fw_bcmdhd.bin"
-WIFI_DRIVER_FW_PATH_AP := "/vendor/firmware/fw_bcmdhd_apsta.bin"
-WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
-WIFI_AVOID_IFACE_RESET_MAC_CHANGE := true
-
 
 $(call inherit-product-if-exists, vendor/huawei/msm8994-common/BoardConfigVendor.mk)
